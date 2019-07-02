d3aeb1948958d910679883f7293073966cb3d944
Zaid Al-Momen <zalmomen01@gmail.com>
2019-07-01 22:23:00 -0500

    Updated Controller for GET and POST endpoints

--- src/test/java/com/proj/todos/todo/TodoRepository.java
+++ src/test/java/com/proj/todos/todo/TodoRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-    List<Todo> findAllById(Iterable<Long> ids);
-}
+    List<Todo> findAll();
+ }
