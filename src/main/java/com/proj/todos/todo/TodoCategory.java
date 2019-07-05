1ed175b65037f38fe7957c5c592abf6be3a4a220
Zaid Al-Momen <zalmomen01@gmail.com>
2019-07-05 13:36:12 -0500

    Added todo category [31]

--- src/main/java/com/proj/todos/todo/TodoCategory.java
+++ src/main/java/com/proj/todos/todo/TodoCategory.java
@@ -1,14 +1,9 @@
 package com.proj.todos.todo;
 
-import java.util.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,20 +18,10 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "todo")
-public class Todo {
-
+@Table(name = "todo_category")
+class TodoCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Long id;
-    @ManyToOne
-    @JoinColumn
-    private TodoCategory todoCategory;
-    @Column(name = "title")
-    private String title;
-    @Column(name = "description")
-    private String description;
-    @Column(name = "created_dt")
-    private Date createdDate;
+    private Integer id;
+    private String category;    
 }
