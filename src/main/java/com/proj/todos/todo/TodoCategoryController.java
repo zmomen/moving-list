9babca53d37bc55d51eb4070e03f692b085e1003
Zaid Al-Momen <zalmomen01@gmail.com>
2019-07-05 17:31:22 -0500

    Added PUT method to update a Todo

--- src/main/java/com/proj/todos/todo/TodoCategoryController.java
+++ src/main/java/com/proj/todos/todo/TodoCategoryController.java
@@ -1,12 +1,8 @@
 package com.proj.todos.todo;
 
-import static org.mockito.ArgumentMatchers.isNull;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/proj/todos/todo/TodoController.java
+++ src/main/java/com/proj/todos/todo/TodoController.java
@@ -7,36 +7,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 @Controller
+@RequestMapping("todos")
 @AllArgsConstructor
 public class TodoController {
 
     @Autowired
     private TodoRepository todoRepository;
 
-    @GetMapping("/todos")
+    @GetMapping
     public @ResponseBody List<Todo> listAll() {
         return todoRepository.findAll();
     }
 
-    @PostMapping("/todos")
+    @PostMapping
     public ResponseEntity<String> createTodo(@RequestBody TodoObject todoObj) {
-        Todo todo = Todo.builder()
-                        .title(todoObj.getTitle())
-                        .description(todoObj.getDescription())
-                        .createdDate(new Date())
-                        .build();
+        Todo todo = Todo.builder().title(todoObj.getTitle()).description(todoObj.getDescription())
+                .createdDate(new Date()).build();
         todoRepository.save(todo);
-        
+
         return ResponseEntity.ok("CREATED!");
 
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateArticle(@PathVariable long id, @RequestBody TodoObject todoObject) {
+        Todo foundTodo = todoRepository.findById(id).get();
+        
+        if (foundTodo == null) {
+            return ResponseEntity.notFound().build();
+        }
+        foundTodo.setTitle(todoObject.getTitle());
+        foundTodo.setDescription(todoObject.getDescription());
+
+        todoRepository.save(foundTodo);
+		return ResponseEntity.ok("Updated!");
     }
 
     @Data(staticConstructor = "of")
