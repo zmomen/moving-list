5a91cee896d2fbb8c02916ca40c0be982d18c020
Zaid Al-Momen <zalmomen01@gmail.com>
2019-08-13 18:37:12 -0500

    WIP moveto intelliJ

--- src/main/java/com/proj/todos/todo/TodoCategory.java
+++ src/main/java/com/proj/todos/todo/TodoCategory.java
@@ -1,25 +1,10 @@
 package com.proj.todos.todo;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
--- src/test/java/com/proj/todos/todo/TodoControllerTest.java
+++ src/test/java/com/proj/todos/todo/TodoControllerTest.java
@@ -1,21 +1,19 @@
 package com.proj.todos.todo;
 
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(TodoController.class)
@@ -29,16 +27,16 @@ public class TodoControllerTest {
     @Test
     public void givenData_whenGetTodoCategories_thenReturnList() throws Exception {
         TodoCategory mockTodoCategory = TodoCategory.builder()
-        .category("category")
-        .build();
-        
+                .category("category")
+                .build();
+
         Todo mockTodo = Todo.builder()
-        .id(1L)
-        .title("title")
-        .todoCategory(mockTodoCategory)
-        .description("description")
-        .createdDate(new Date())
-        .build();
+                .id(1L)
+                .title("title")
+                .todoCategory(mockTodoCategory)
+                .description("description")
+                .createdDate(new Date())
+                .build();
 
         List<Todo> expected = Arrays.asList(mockTodo);
         when(todoRepository.findAll()).thenReturn(expected);
