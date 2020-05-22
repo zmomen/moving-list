38146be4e00469456aa1caa54215151f826bd699
Zaid Al-Momen <zalmomen01@gmail.com>
2020-05-21 23:59:21 -0500

    Added new icons for edit/delete/ccomplete a moving task. more styling. Added completed tasks api call

--- ui/src/todo/TodoList.js
+++ ui/src/todo/TodoList.js
@@ -1,63 +1,83 @@
 import React, { useState } from "react";
 import { dateFmt } from "../utils/helpers";
+import "./Todo.css";
+import Delete from "../common/icons/Delete";
+import CheckMark from "../common/icons/CheckMark";
+import Edit from "../common/icons/Edit";
 
-const TodoList = props => {
-  const [isComplete, setTodoState] = useState(false);
-  const [whichIndex, setIndex] = useState(-1);
-  const headers = ["#", "Title", "Description", "Modified date", "Action"];
+const TodoList = (props) => {
+  const headers = ["#", "Title", "Description", "Modified date", "Actions"];
+
+  const handleDelete = (todo) => {
+    console.warn("Deleted!", todo); 
+    
+  };
+
   return (
-    <div>
-      {props.data.map((element, index) => {
-        return (
-          <div key={index}>
-            <b>{element.category}</b>
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  {headers.map((element, index) => (
-                    <th key={index}>
-                      {element.charAt(0).toUpperCase() + element.slice(1)}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {element.todos.map((todo, index) => {
-                  const rowStyle =
-                    whichIndex === index && isComplete
-                      ? { textDecoration: "line-through" }
-                      : {};
-                  return (
-                    <tr
-                      key={index}
-                      className={index % 2 === 0 ? "active" : ""}
-                      style={rowStyle}
-                    >
-                      <td>{todo.id}</td>
-                      <td>{todo.title}</td>
-                      <td>{todo.description}</td>
-                      <td>{dateFmt(todo.modifiedDate)}</td>
-                      <td>
-                        <span
-                          onClick={() => {
-                            setTodoState(!isComplete);
-                            setIndex(index);
-                          }}
-                          style={{ cursor: "pointer", color: "blue" }}
-                        >
-                          &#10003;
-                        </span>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className={"todo-table-title"}>Moving Checklist</div>
+      <div className={"todo-table"}>
+        {props.data.map((element, index) => {
+          return (
+            <div key={index}>
+              <b className={"todo-category"}>{element.category}</b>
+              <table className={"table table-striped"}>
+                <thead>
+                  <tr>
+                    {headers.map((element, hd) => (
+                      <th key={hd}>
+                        {element.charAt(0).toUpperCase() + element.slice(1)}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {element.todos.map((todo, idx) => {
+                    return (
+                      <>
+                        <tr key={idx} className={idx % 2 === 0 ? "active" : ""}>
+                          <td style={{ width: "50px" }}>{todo.id}</td>
+                          <td style={{ width: "180px" }}>{todo.title}</td>
+                          <td style={{ width: "414px" }}>{todo.description}</td>
+                          <td style={{ width: "200px" }}>
+                            {dateFmt(todo.modifiedDate)}
+                          </td>
+                          <td style={{ width: "100px" }}>
+                            <span
+                              onClick={() => handleDelete(todo)}
+                              className={"todo-action-btn"}
+                            >
+                              <Delete />
+                            </span>
+                            <span
+                              className={"todo-action-btn"}
+                              onClick={() => console.warn("checked")}
+                            >
+                              <CheckMark />
+                            </span>
+                            <span
+                              className={"todo-action-btn"}
+                              onClick={() => console.warn("edited")}
+                            >
+                              <Edit />
+                            </span>
+                          </td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
 export default TodoList;
+
+const Checkbox = ({ name, checked, onChange }) => (
+  <input type="checkbox" name={name} checked={checked} onChange={onChange} />
+);
