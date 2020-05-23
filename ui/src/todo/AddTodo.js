f33011fa4ae4a8d3a84bfd49c5e58e98f017b172
zmomen <zalmomen01@gmail.com>
2020-05-22 19:12:52 -0500

    Add input validation

--- ui/src/todo/AddTodo.js
+++ ui/src/todo/AddTodo.js
@@ -1,7 +1,9 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import "./Todo.css";
+import ErrorBlock from "../common/ErrorBlock";
 
 const AddTodo = (props) => {
+  const [errMsg, setErrMsg] = useState("");
   const [userInput, setUserInput] = useReducer(
     (state, newState) => ({ ...state, ...newState }),
     {
@@ -11,17 +13,30 @@ const AddTodo = (props) => {
     }
   );
 
+  const isInputValid = () => {
+    if (
+      userInput["title"] === "" ||
+      userInput["description"] === "" ||
+      userInput["category"] === ""
+    ) {
+      setErrMsg("Invalid input! Please retry.");
+      return false;
+    }
+    return true;
+  };
+
   const handleChange = (evt) => {
     const name = evt.target.name;
     const newValue = evt.target.value;
     setUserInput({ [name]: newValue });
+    setErrMsg("");
   };
   return (
     <div className={"add-btn black"}>
-      <div className={"form-group"}>
+      <div className={"form-group black"}>
         <label>Title</label>
         <input
-          className="form-input"
+          className={"form-input black"}
           type="text"
           name="title"
           value={userInput.title}
@@ -29,7 +44,7 @@ const AddTodo = (props) => {
         />
         <label>Category</label>
         <input
-          className="form-input"
+          className={"form-input black"}
           type="text"
           name="category"
           value={userInput.category}
@@ -38,7 +53,7 @@ const AddTodo = (props) => {
         <label>Description</label>
         <div className={"margins"}>
           <textarea
-            className={"form-input"}
+            className={"form-input black"}
             name="description"
             value={userInput.description}
             onChange={handleChange}
@@ -47,10 +62,11 @@ const AddTodo = (props) => {
       </div>
       <button
         className={"btn btn-success"}
-        onClick={() => props.addTodo(userInput)}
+        onClick={() => isInputValid() && props.addTodo(userInput)}
       >
         Add Todo
       </button>
+      {errMsg !== "" && <ErrorBlock message={errMsg} />}
     </div>
   );
 };
